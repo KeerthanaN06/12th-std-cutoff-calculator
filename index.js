4d98358455d9ce9908e5783474ff90100afd198b
Keerthana N <133489674+KeerthanaN06@users.noreply.github.com>
2024-02-04 22:45:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import  ReactDOM  from "react-dom";
+import Calc from "./CutOffCalculator"; 
+import "./stylecalc.css";
+
+ ReactDOM.render(<Calc/>,document.getElementById("root"));
+ 
+
+
+ // import "./style2counter.css";
+// const myelement= <Parentform/>
+// function App()
+// {
+//   return(
+//   <calc/>
+//   )
+  
+// }
+// const root=ReactDOM.createRoot(document.getElementById("root"));
+// root.render(<calc/>);
